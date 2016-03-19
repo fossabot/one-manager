b9fa6c7cf5b8ccebea3e13d037c6d857452b0002
kucuny <kucuny@gmail.com>
2016-03-19 10:26:44 +0900

    Modify classes, school urls.py

--- api/classes/urls.py
+++ api/classes/urls.py
@@ -1,18 +1,22 @@
 from django.conf.urls import url, include
-from rest_framework_nested.routers import SimpleRouter, NestedSimpleRouter
+from rest_framework_nested.routers import SimpleRouter, DefaultRouter, NestedSimpleRouter
 from classes.views import ClassesViewSet, SemesterViewSet
 from classes.views import ClassesSemesterViewSet
-from student.views import StudentViewSet
 from student.views import ClassesStudentViewSet
 
+root_router = DefaultRouter()
+root_router.register('classes', ClassesViewSet, 'classes')
+root_router.register('semester', SemesterViewSet, 'semester')
+
 classes_router = SimpleRouter()
-classes_router.register('classes', ClassesViewSet)
+classes_router.register('classes', ClassesViewSet, base_name='list-classes')
 classes_semester_router = NestedSimpleRouter(classes_router, 'classes', lookup='classes')
-classes_semester_router.register('semester', ClassesSemesterViewSet, base_name='classes-semester')
+classes_semester_router.register('semester', ClassesSemesterViewSet, base_name='list-classes-semester')
 classes_student_router = NestedSimpleRouter(classes_router, 'classes', lookup='classes')
-classes_student_router.register('student', ClassesStudentViewSet, base_name='classes-student')
+classes_student_router.register('student', ClassesStudentViewSet, base_name='list-classes-student')
 
 urlpatterns = [
+    url(r'', include(root_router.urls)),
     url(r'list/', include(classes_router.urls)),
     url(r'list/', include(classes_semester_router.urls)),
     url(r'list/', include(classes_student_router.urls)),
